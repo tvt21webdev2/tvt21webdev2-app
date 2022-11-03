b02abd55ce947287a54c0f552b1276885bddfdf3
Olli Petäjäniemi <petajaol@gmail.com>
2022-11-03 11:37:31 +0200

    initialized react project and renamed java project

---
+++ api/src/main/java/com/tvt21webdev2/climatechangecharts/ClimateChangeChartsApplication.java
@@ -0,0 +1,13 @@
+package com.tvt21webdev2.climatechangecharts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimateChangeChartsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimateChangeChartsApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/tvt21webdev2/climatechangecharts/ClimateChangeChartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tvt21webdev2.climatechangecharts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimateChangeChartsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
