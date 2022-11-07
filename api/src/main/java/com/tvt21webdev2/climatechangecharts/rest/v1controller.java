0b313731d56042241cf47e59083fb62ee61d0c29
Johannes Rantapää <johannes.rantapaa@gmail.com>
2022-11-07 11:22:39 +0200

    created alternate way of handling v1 getmap

--- api/src/main/java/com/tvt21webdev2/climatechangecharts/rest/v1controller.java
+++ api/src/main/java/com/tvt21webdev2/climatechangecharts/rest/v1controller.java
@@ -1,5 +1,6 @@
 package com.tvt21webdev2.climatechangecharts.rest;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,10 +18,10 @@ public class v1controller {
     @Autowired
     private v1repo v1repo; 
 
-    @GetMapping("/v1")
-    public List<v1> getData() {
-        return v1repo.findAll();
-    }
+    // @GetMapping("/v1")
+    // public List<v1> getData() {
+    //     return v1repo.findAll();
+    // }
 
     @GetMapping("v1/type/{type}")
     public List<v1> getByType(@PathVariable String type) {
@@ -45,5 +46,21 @@ public class v1controller {
     @GetMapping("v1/search/{type}/{location}/{year}")
     public List<v1> getByQuery(@PathVariable String type, @PathVariable String location, @PathVariable String year) {
         return v1repo.findByTypeAndLocationAndYear(type, location, year);
+    }
+
+    // http://localhost:8080/v1?type=monthly&location=global&year=2022-01
+    @GetMapping("/v1")
+    public List<v1> getData(@RequestParam(defaultValue = "empty") String type, @RequestParam(defaultValue = "empty") String location, @RequestParam(defaultValue = "empty") String year) {
+        if (!type.equals("empty") && !location.equals("empty") && !year.equals("empty"))
+            return v1repo.findByTypeAndLocationAndYear(type, location, year);
+        if (!type.equals("empty") && !location.equals("empty"))
+            return v1repo.findByTypeAndLocationOrderByYear(type, location);
+        if (!type.equals("empty"))
+            return v1repo.findByType(type);
+        if (!location.equals("empty"))
+            return v1repo.findByLocation(location);
+        if (!year.equals("empty"))
+            return v1repo.findByYear(year);
+        return v1repo.findAll();
     }
 }
