25181e5bacbe214875b48baef97af4fd0d31b11f
Johannes Rantapää <johannes.rantapaa@gmail.com>
2022-11-07 11:29:58 +0200

    removed unnecessary imports

--- api/src/main/java/com/tvt21webdev2/climatechangecharts/rest/v1controller.java
+++ api/src/main/java/com/tvt21webdev2/climatechangecharts/rest/v1controller.java
@@ -1,11 +1,9 @@
 package com.tvt21webdev2.climatechangecharts.rest;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
