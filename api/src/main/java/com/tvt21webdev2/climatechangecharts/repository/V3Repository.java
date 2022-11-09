e3385618cbf0576517914fbf8a743454eea11d69
Johannes Rantapää <johannes.rantapaa@gmail.com>
2022-11-09 14:22:10 +0200

    refactor

--- api/src/main/java/com/tvt21webdev2/climatechangecharts/repository/V3Repository.java
+++ api/src/main/java/com/tvt21webdev2/climatechangecharts/repository/V3Repository.java
@@ -3,7 +3,6 @@ package com.tvt21webdev2.climatechangecharts.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.tvt21webdev2.climatechangecharts.data.V3;
