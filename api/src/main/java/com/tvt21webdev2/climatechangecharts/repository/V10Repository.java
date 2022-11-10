bfeadaeb3232fb6a716ac7f2ae9b63ae13a4d8ae
tpoldsam <teea.poldsam@gmail.com>
2022-11-10 15:10:20 +0200

    #33 Add v10 data, controller, repository and service classes

---
+++ api/src/main/java/com/tvt21webdev2/climatechangecharts/repository/V10Repository.java
@@ -0,0 +1,20 @@
+package com.tvt21webdev2.climatechangecharts.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.tvt21webdev2.climatechangecharts.data.V10;
+
+@Repository
+public interface V10Repository extends JpaRepository<V10, Long> {
+    
+    @Override
+    @Query("SELECT v FROM V10 ORDER BY years_ago")
+    List <V10> findAll();
+
+    @Query("SELECT v FROM V10 v WHERE v.years_ago >= ?1 ORDER BY year")
+    List<V10> findByYear(int year);
+}
