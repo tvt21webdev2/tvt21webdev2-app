c7328b0b8e5dc8b642b522d686db12c413ad146f
Olli Petäjäniemi <petajaol@gmail.com>
2022-11-14 15:04:21 +0200

    add view data, repository, service, controller

---
+++ api/src/main/java/com/tvt21webdev2/climatechangecharts/repository/ViewRepository.java
@@ -0,0 +1,20 @@
+package com.tvt21webdev2.climatechangecharts.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.tvt21webdev2.climatechangecharts.data.View;
+
+@Repository
+public interface ViewRepository extends JpaRepository<View, Long> {
+
+  @Query("SELECT v FROM View v WHERE v.userId = ?1")
+  List<View> findByUserId(Long userId);
+
+  @Query("SELECT v FROM View v WHERE v.url = ?1")
+  List<View> findByUrl(String url);
+
+}
