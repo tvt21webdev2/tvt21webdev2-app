af0ffa1e4adefc974cdd1f73d45d86793c1d4905
Johannes Rantapää <johannes.rantapaa@gmail.com>
2022-11-15 11:08:15 +0200

    add BCrypt

---
+++ api/src/main/java/com/tvt21webdev2/climatechangecharts/service/EncoderService.java
@@ -0,0 +1,9 @@
+package com.tvt21webdev2.climatechangecharts.service;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EncoderService extends BCryptPasswordEncoder {
+  
+}
