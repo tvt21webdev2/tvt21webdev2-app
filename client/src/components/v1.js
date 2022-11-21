d9c01dd073d156b00a0130c949dbb7370a1df894
Johannes Rantapää <johannes.rantapaa@gmail.com>
2022-11-21 15:02:50 +0200

    add v1 chart

--- client/src/components/v1.js
+++ client/src/components/v1.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { Chart } from "chart.js/auto";
 import { Line } from "react-chartjs-2";
-import { DateTime } from 'luxon'
+// import { DateTime } from 'luxon'
 
 const urlAnnualGlobal = 'http://localhost:8080/v1?type=annual&location=global'
 const urlAnnualNorthern = 'http://localhost:8080/v1?type=annual&location=northern'
@@ -10,6 +10,7 @@ const urlAnnualSouthern = 'http://localhost:8080/v1?type=annual&location=souther
 const urlMonthlyGlobal = 'http://localhost:8080/v1?type=monthly&location=global'
 const urlMonthlyNorthern = 'http://localhost:8080/v1?type=monthly&location=northern'
 const urlMonthlySouthern = 'http://localhost:8080/v1?type=monthly&location=southern'
+const urlV2Data = 'http://localhost:8080/v2'
 
 export default function V1() {
   const [v1DataAnnualGlobal, setV1DataAnnualGlobal] = useState([])
@@ -18,156 +19,201 @@ export default function V1() {
   const [v1DataMonthlyGlobal, setV1DataMonthlyGlobal] = useState([])
   const [v1DataMonthlyNorthern, setV1DataMonthlyNorthern] = useState([])
   const [v1DataMonthlySouthern, setV1DataMonthlySouthern] = useState([])
+  const [v2Data, setV2Data] = useState([])
+  const [isLoaded, setIsLoaded] = useState(false)
 
   useEffect(() => {
-    axios.get(urlAnnualGlobal)
-    .then(response => {
-      let temp = []
-      response.data.forEach(element => {
-        temp.push(element)
-      });
-      setV1DataAnnualGlobal(temp)
-    }).catch(err => {
-      console.log(err);
-    })
+    if (!isLoaded) {
+      axios.get(urlAnnualGlobal)
+      .then(response => {
+        let temp = []
+        response.data.forEach(element => {
+          let parsed = new Date(element.year).toDateString().split(' ')
+          element.parsedDate = parsed[1] + ' ' + parsed[3];
+          temp.push(element)
+        });
+        setV1DataAnnualGlobal(temp)
+      }).catch(err => {
+        console.log(err);
+      })
 
-    axios.get(urlAnnualNorthern)
-    .then(response => {
-      let temp = []
-      response.data.forEach(element => {
-        temp.push(element)
-      });
-      setV1DataAnnualNorthern(temp)
-    }).catch(err => {
-      console.log(err);
-    })
+      axios.get(urlAnnualNorthern)
+      .then(response => {
+        let temp = []
+        response.data.forEach(element => {
+          let parsed = new Date(element.year).toDateString().split(' ')
+          element.parsedDate = parsed[1] + ' ' + parsed[3]
+          temp.push(element)
+        });
+        setV1DataAnnualNorthern(temp)
+      }).catch(err => {
+        console.log(err);
+      })
 
-    axios.get(urlAnnualSouthern)
-    .then(response => {
-      let temp = []
-      response.data.forEach(element => {
-        temp.push(element)
-      });
-      setV1DataAnnualSouthern(temp)
-    }).catch(err => {
-      console.log(err);
-    })
+      axios.get(urlAnnualSouthern)
+      .then(response => {
+        let temp = []
+        response.data.forEach(element => {
+          let parsed = new Date(element.year).toDateString().split(' ')
+          element.parsedDate = parsed[1] + ' ' + parsed[3]
+          temp.push(element)
+        });
+        setV1DataAnnualSouthern(temp)
+      }).catch(err => {
+        console.log(err);
+      })
+  
+      axios.get(urlMonthlyGlobal)
+      .then(response => {
+        let temp = []
+        response.data.forEach(element => {
+          let parsed = new Date(element.year).toDateString().split(' ')
+          element.parsedDate = parsed[1] + ' ' + parsed[3]
+          temp.push(element)
+        });
+        setV1DataMonthlyGlobal(temp)
+      }).catch(err => {
+        console.log(err);
+      })
 
-    axios.get(urlMonthlyGlobal)
-    .then(response => {
-      let temp = []
-      response.data.forEach(element => {
-        temp.push(element)
-      });
-      setV1DataMonthlyGlobal(temp)
-      console.log(temp);
-    }).catch(err => {
-      console.log(err);
-    })
+      axios.get(urlMonthlyNorthern)
+      .then(response => {
+        let temp = []
+        response.data.forEach(element => {
+          let parsed = new Date(element.year).toDateString().split(' ')
+          element.parsedDate = parsed[1] + ' ' + parsed[3]
+          temp.push(element)
+        });
+        setV1DataMonthlyNorthern(temp)
+      }).catch(err => {
+        console.log(err);
+      })
 
-    axios.get(urlMonthlyNorthern)
-    .then(response => {
-      let temp = []
-      response.data.forEach(element => {
-        temp.push(element)
-      });
-      setV1DataMonthlyNorthern(temp)
-    }).catch(err => {
-      console.log(err);
-    })
+      axios.get(urlMonthlySouthern)
+      .then(response => {
+        let temp = []
+        response.data.forEach(element => {
+          let parsed = new Date(element.year).toDateString().split(' ')
+          element.parsedDate = parsed[1] + ' ' + parsed[3]
+          temp.push(element)
+        });
+        setV1DataMonthlySouthern(temp)
+      }).catch(err => {
+        console.log(err);
+      })
 
-    axios.get(urlMonthlySouthern)
-    .then(response => {
-      let temp = []
-      response.data.forEach(element => {
-        temp.push(element)
-      });
-      setV1DataMonthlySouthern(temp)
-    }).catch(err => {
-      console.log(err);
-    })
+      axios.get(urlV2Data)
+      .then(response => {
+        let temp = []
+        response.data.forEach(element => {
+          // let parsed = new Date(element.year).toDateString().split(' ')
+          // element.parsedDate = parsed[1] + ' ' + parsed[3]
+          temp.push(element)
+        });
+        setV2Data(temp)
+      }).catch(err => {
+        console.log(err);
+      })
+      
+      setIsLoaded(true)
+    }
   }, [])
-
+  
+  console.log(v2Data);
+  
   const data = {
     datasets: [
-      // {
-      //   label: "Annual global",
-      //   data: [...v1DataAnnualGlobal],
-      //   borderColor: "rgb(0, 0, 0)",
-      //   backgroundColor: "rgba(0, 0, 0, 0.5)",
-      //   xAxisID: "x",
-      //   yAxisID: "y",
-      //   parsing: {
-      //     xAxisKey: "year",
-      //     yAxisKey: "anomaly",
-      //   },
-      //   pointRadius: 1,
-      // },
       {
-        label: "Monthly global",
-        data: [...v1DataMonthlyGlobal],
-        borderColor: "rgb(0, 0, 0)",
-        backgroundColor: "rgba(0, 0, 0, 0.5)",
-        xAxisID: "xAxis",
-        yAxisID: "yAxis",
+        label: "2000 year temperatures",
+        data: v2Data,
+        borderColor: "rgba(255, 0, 0, 1)",
+        backgroundColor: "rgba(255, 0, 0, 0.2)",
+        xAxisID: "date2k",
+        yAxisID: "temp",
         parsing: {
           xAxisKey: "year",
           yAxisKey: "anomaly",
         },
-        pointRadius: 1,
+        pointRadius: 2,
       },
-      // {
-      //   label: "Annual northern",
-      //   data: [...v1DataAnnualNorthern],
-      //   borderColor: "rgb(255, 0, 0)",
-      //   backgroundColor: "rgba(255, 0, 0, 0.3)",
-      //   xAxisID: "x",
-      //   yAxisID: "y",
-      //   parsing: {
-      //     xAxisKey: "year",
-      //     yAxisKey: "anomaly",
-      //   },
-      //   pointRadius: 1,
-      // },
-      // {
-      //   label: "Monthly northern",
-      //   data: [...v1DataMonthlyNorthern],
-      //   borderColor: "rgb(255, 0, 0)",
-      //   backgroundColor: "rgba(255, 0, 0, 0.3)",
-      //   xAxisID: "x",
-      //   yAxisID: "y",
-      //   parsing: {
-      //     xAxisKey: "year",
-      //     yAxisKey: "anomaly",
-      //   },
-      //   pointRadius: 1,
-      // },
-      // {
-      //   label: "Annual southern",
-      //   data: [...v1DataAnnualSouthern],
-      //   borderColor: "rgb(0, 0, 255)",
-      //   backgroundColor: "rgba(0, 0, 255, 0.3)",
-      //   xAxisID: "x",
-      //   yAxisID: "y",
-      //   parsing: {
-      //     xAxisKey: "year",
-      //     yAxisKey: "anomaly",
-      //   },
-      //   pointRadius: 1,
-      // },
-      // {
-      //   label: "Monthly southern",
-      //   data: [...v1DataMonthlySouthern],
-      //   borderColor: "rgb(0, 0, 255)",
-      //   backgroundColor: "rgba(0, 0, 255, 0.3)",
-      //   xAxisID: "x",
-      //   yAxisID: "y",
-      //   parsing: {
-      //     xAxisKey: "year",
-      //     yAxisKey: "anomaly",
-      //   },
-      //   pointRadius: 1,
-      // },
+      {
+        label: "Monthly global",
+        data: v1DataMonthlyGlobal,
+        borderColor: "rgba(0, 0, 0, 0.5)",
+        backgroundColor: "rgba(0, 0, 0, 0.5)",
+        xAxisID: "date",
+        yAxisID: "temp",
+        parsing: {
+          xAxisKey: "parsedDate",
+          yAxisKey: "anomaly",
+        },
+        pointRadius: 2,
+      },
+      {
+        label: "Monthly northern hemisphere",
+        data: v1DataMonthlyNorthern,
+        borderColor: "rgba(0, 0, 255, 0.5)",
+        backgroundColor: "rgba(0, 0, 255, 0.4)",
+        xAxisID: "date",
+        yAxisID: "temp",
+        parsing: {
+          xAxisKey: "parsedDate",
+          yAxisKey: "anomaly",
+        },
+        pointRadius: 2,
+      },
+      {
+        label: "Monthly southern hemisphere",
+        data: v1DataMonthlySouthern,
+        borderColor: "rgba(255, 0, 0, 0.5)",
+        backgroundColor: "rgba(255, 0, 0, 0.4)",
+        xAxisID: "date",
+        yAxisID: "temp",
+        parsing: {
+          xAxisKey: "parsedDate",
+          yAxisKey: "anomaly",
+        },
+        pointRadius: 2,
+      },
+      {
+        label: "Annual global",
+        data: v1DataAnnualGlobal,
+        borderColor: "rgba(0, 0, 0, 0.3)",
+        backgroundColor: "rgba(0, 0, 0, 0.5)",
+        xAxisID: "date",
+        yAxisID: "temp",
+        parsing: {
+          xAxisKey: "parsedDate",
+          yAxisKey: "anomaly",
+        },
+        pointRadius: 2,
+      },
+      {
+        label: "Annual northern hemisphere",
+        data: v1DataAnnualNorthern,
+        borderColor: "rgba(0, 0, 255, 0.5)",
+        backgroundColor: "rgba(0, 0, 255, 0.4)",
+        xAxisID: "date",
+        yAxisID: "temp",
+        parsing: {
+          xAxisKey: "parsedDate",
+          yAxisKey: "anomaly",
+        },
+        pointRadius: 2,
+      },
+      {
+        label: "Annual southern hemisphere",
+        data: v1DataAnnualSouthern,
+        borderColor: "rgba(255, 0, 0, 0.5)",
+        backgroundColor: "rgba(255, 0, 0, 0.4)",
+        xAxisID: "date",
+        yAxisID: "temp",
+        parsing: {
+          xAxisKey: "parsedDate",
+          yAxisKey: "anomaly",
+        },
+        pointRadius: 2,
+      },
     ],
   };
 
@@ -175,7 +221,10 @@ export default function V1() {
     responsive: true,
     plugins: {
       legend: {
-        position: "top",
+        labels: {
+          usePointStyle: true,
+          pointStyle: "rectRounded",
+        }
       },
       title: {
         display: true,
@@ -183,33 +232,35 @@ export default function V1() {
       },
     },
     scales: {
-      yAxis: {
+      temp: {
         type: "linear",
         display: true,
         position: "right",
       },
-      xAxis: [ {
+      // date: {
+      //   display: true,
+      //   position: 'bottom',
+      // },
+      date2k: {
+        type: "linear",
         display: true,
-        type: 'time',
-        time: {
-          parser: 'YYYY-MM',
-          tooltipFormat: 'YYYY MMM',
-          unit: 'year',
-          unitStepSize: 1,
-          displayFormats: {
-            'day': 'MM/YYYY'
-          }
-        }
-      }
-    ],
+        position: "bottom",
+      },
     },
   };
 
-  return (
-    <div>
-      <div style={{ width: "1000px" }}>
+  if (!isLoaded) {
+    return (
+      <div>
+        <p>Fetching data</p>
+      </div>
+    )
+  } else {
+    return (
+      <div style={{ width: "1500px" }}>
         <Line options={options} data={data}/>
       </div>
-    </div>
-  )
+    )
+  }
+
 }
