f47a12f37aada178b1fafcf211cf3ca7610f6b7f
Johannes Rantapää <johannes.rantapaa@gmail.com>
2022-11-23 15:26:36 +0200

    edit css

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+
 export default function Homepage() {
   return (
     <div>
-      Homepage
       <Link to='/v1'>V1 CHART</Link> 
       <Link to='/v5'>V5 CHART</Link>
     </div>
--- client/src/components/v1.js
+++ client/src/components/v1.js
@@ -283,7 +283,7 @@ export default function V1() {
           }
           handleResetZoom()
         }
-      }
+      },
     },
     scales: {
       x: {
