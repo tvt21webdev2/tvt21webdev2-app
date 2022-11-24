10b564c782d703544356eef19cc5c346ac636de9
Johannes Rantapää <johannes.rantapaa@gmail.com>
2022-11-24 14:36:54 +0200

    restructuring

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -7,6 +7,8 @@ export default function Homepage() {
     <div>
       <Link to='/v1'>V1 CHART</Link> 
       <Link to='/v5'>V5 CHART</Link>
+      <Link to='/v8'>V8 CHART</Link>
+      <Link to='/v9'>V9 CHART</Link>
     </div>
   )
 }
