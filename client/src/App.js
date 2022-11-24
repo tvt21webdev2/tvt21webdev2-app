e5e119162f688f6e11981f9f8fec1438517aad19
petajaol <petajaol@gmail.com>
2022-11-24 13:31:57 +0200

    navbar template, remove unnecessary react project default  files

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import V8 from "./V8";
-import V9 from "./V9";
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <V8/>
-      <V9/>
-    </div>
+    <Box sx={{flexGrow: 1}}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
+            Home
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
-
-export default App;
