eb98c4b971808637495c2e3b337fea7c26fe7327
tpoldsam <teea.poldsam@gmail.com>
2022-11-30 13:34:51 +0200

    Add n2 to pages

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom'
 import Frontpage from './pages/Frontpage'
 import Navbar from './components/Navbar';
+import N2 from './pages/N2';
 import V1 from './components/V1'
 import V5 from './components/V5';
+import V6 from './components/V6';
+import V7 from './components/V7';
 import V8 from './components/V8';
 import V9 from './components/V9';
 import './styles/App.css';
@@ -15,8 +18,11 @@ export default function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Frontpage />} />
+        <Route path="/n2" element={<N2 />} />
         <Route path="/v1" element={<V1 />} />
         <Route path="/v5" element={<V5 />} />
+        <Route path="/v6" element={<V6 />} />
+        <Route path="/v7" element={<V7 />} />
         <Route path="/v8" element={<V8 />} />
         <Route path="/v9" element={<V9 />} />
       </Routes>
---
+++ client/src/pages/N2.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import V8 from '../components/V8';
+import V9 from '../components/V9';
+
+function N2() {
+  return (
+    <div>
+      <V8/>
+      <V9/>
+    </div>
+  );
+}
+
+export default N2;
