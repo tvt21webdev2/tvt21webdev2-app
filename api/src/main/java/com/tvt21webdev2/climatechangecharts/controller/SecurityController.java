447db3bd6a5b682caef8349b707fd2f309e727ec
petajaol <petajaol@gmail.com>
2022-11-30 11:34:34 +0200

    update controller and tests for password matching in singup

--- api/src/main/java/com/tvt21webdev2/climatechangecharts/controller/SecurityController.java
+++ api/src/main/java/com/tvt21webdev2/climatechangecharts/controller/SecurityController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.tvt21webdev2.climatechangecharts.data.User;
 import com.tvt21webdev2.climatechangecharts.service.SecurityService;
+
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -23,19 +25,23 @@ public class SecurityController {
   }
 
   @PostMapping("/register")
-  public ResponseEntity<String> saveUser(@RequestBody User user) {
+  public ResponseEntity<String> saveUser(@RequestBody Map<String, String> userMap) {
+    User user = new User(userMap.get("username"), userMap.get("password"));
     Pattern passwordRegex = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$");
     Pattern usernameRegex = Pattern.compile("^[a-zA-Z0-9]{4,16}$");
     Matcher passwordMatcher = passwordRegex.matcher(user.getPassword());
     Matcher usernameMatcher = usernameRegex.matcher(user.getUsername());
-    if (!usernameMatcher.find()) {
-      return new ResponseEntity<>("Username should contain 4 to 16 alphanumeric characters", HttpStatus.BAD_REQUEST);
-    }
     if (service.checkIfUserExists(user)) {
       return new ResponseEntity<>("Username already exists", HttpStatus.BAD_REQUEST);
     }
+    if (!usernameMatcher.find()) {
+      return new ResponseEntity<>("Username should contain 4 to 16 alphanumeric characters", HttpStatus.BAD_REQUEST);
+    }
     if (!passwordMatcher.find()) {
       return new ResponseEntity<>("Password should contain at least 8 characters, including one uppercase letter, one lowercase letter and one number", HttpStatus.BAD_REQUEST);
+    }
+    if (!user.getPassword().equals(userMap.get("passwordAgain"))) {
+      return new ResponseEntity<>("Passwords do not match", HttpStatus.BAD_REQUEST);
     }
     service.saveUser(user);
     return new ResponseEntity<>(user.getUsername() + " registered successfully", HttpStatus.CREATED);
