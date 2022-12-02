b648a69c0338f6e5d8d34378343b35937fac7282
tpoldsam <teea.poldsam@gmail.com>
2022-12-02 15:37:41 +0200

    add changes to n2 and n2 styling

--- client/src/pages/N2.js
+++ client/src/pages/N2.js
@@ -2,21 +2,34 @@ import React from 'react'
 import V8 from '../components/V8';
 import V9 from '../components/V9';
 import '../styles/n2.css';
+import video from '../images/cherry.mp4';
 
 function N2() {
   return (
-    <div className='container'>
-      <div>
-        <h1>keskitetty n2 otsikko</h1>
-        <div id='v8'>
-          <h2>keskitetty otsikko tänne?</h2>
-          <V8 />
-        </div>
-        <div id='v9'>
-          <h2>keskitetty otsikko tänne? (^^lisää väliä!)</h2>
-          <V9 />
-        </div>
+    <div className='container-n2'>
+      <video autoPlay loop muted playsInline className="back-video">
+        <source src={video} type="video/mp4"></source>
+      </video>
+
+      <div className='content-n2'>
+        <h1>Päästölähteet</h1>
+        <p>Kuvaus: lorem</p>
       </div>
+
+      <div id='v8'>
+        <h2>Hiilidioksidipäästöt valtion mukaan</h2>
+        <p>Kuvaus: lorem</p>
+        <V8 />
+      </div>
+
+      <div id='v9'>
+        <h2>Hiilidioksidipäästöt sektoreittain</h2>
+        <p>Piirakkakaavio co2 päästöistä toimialoittain. Sektoria klikkaamalla voidaan tarkastella alasektoreita. Painamalla kuvaajaa hiiren oikealla painikkeella</p>
+        <V9 />
+      </div>
+
+      
+
     </div>
   );
 }
