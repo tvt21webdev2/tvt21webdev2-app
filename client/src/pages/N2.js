c1fcf98d4e379bf020c290f6baad4d181fae986c
tpoldsam <teea.poldsam@gmail.com>
2022-12-07 14:48:52 +0200

    fix formatting and add some odd changes

--- client/src/pages/N2.js
+++ client/src/pages/N2.js
@@ -17,8 +17,8 @@ function N2() {
 
       <div id='v8'>
         <h2>Hiilidioksidipäästöt valtion mukaan</h2>
-        <p>Pinottu viivakaavio valtiokohtaisista hiilidioksidipäästöistä. <br/>
-          Tutkimuksen<a href="https://data.icos-cp.eu/licence_accept?ids=%5B%22lApekzcmd4DRC34oGXQqOxbJ%22%5D"> data.</a> <br/>
+        <p>Pinottu viivakaavio valtiokohtaisista hiilidioksidipäästöistä. <br />
+          Tutkimuksen<a href="https://data.icos-cp.eu/licence_accept?ids=%5B%22lApekzcmd4DRC34oGXQqOxbJ%22%5D"> data.</a> <br />
           Tutkimuksen<a href="https://essd.copernicus.org/articles/14/1917/2022/"> kuvaus.</a>
         </p>
         <V8 />
@@ -26,18 +26,15 @@ function N2() {
 
       <div id='v9'>
         <h2>Hiilidioksidipäästöt sektoreittain</h2>
-        <p>Piirakkakaavio co2 päästöistä toimialoittain. 
-          Sektoria klikkaamalla voidaan tarkastella alasektoreita. <br/>
-          Painamalla kuvaajaa hiiren oikealla painikkeella voidaan palata edelliseen sektorinäkymään. <br/>
+        <p>Piirakkakaavio co2 päästöistä toimialoittain.
+          Sektoria klikkaamalla voidaan tarkastella alasektoreita. <br />
+          Painamalla kuvaajaa hiiren oikealla painikkeella voidaan palata edelliseen sektorinäkymään. <br />
           Tutkimuksen<a href="https://ourworldindata.org/emissions-by-sector#co2-emissions-by-sector"> kuvaus.</a>
         </p>
-        <div id='help'>
+        <div id='helper'>
           <V9 />
         </div>
       </div>
-
-      
-
     </div>
   );
 }
