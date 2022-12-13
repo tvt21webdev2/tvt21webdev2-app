87738d763270e1868ec557ea37e470ce54a470b6
petajaol <petajaol@gmail.com>
2022-12-13 15:14:24 +0200

    change mysql driver to postgres, edit User entity table name

--- api/src/main/java/com/tvt21webdev2/climatechangecharts/data/User.java
+++ api/src/main/java/com/tvt21webdev2/climatechangecharts/data/User.java
@@ -1,12 +1,9 @@
 package com.tvt21webdev2.climatechangecharts.data;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "user_account")
 public class User {
 
   @Id
